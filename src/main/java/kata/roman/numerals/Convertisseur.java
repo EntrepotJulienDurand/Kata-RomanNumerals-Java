89b714e70bc23fa4a39c64bd3245749ef995b278
JDURAND <julien19durand@hotmail.fr>
2014-10-07 14:23:50 +0200

    implémentation avec test acceptation 4888

--- src/main/java/kata/roman/numerals/Convertisseur.java
+++ src/main/java/kata/roman/numerals/Convertisseur.java
@@ -1,5 +1,38 @@
 package kata.roman.numerals;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+
 public class Convertisseur {
 
+	private static Map<Integer, String> correspondanceArabeRomain = new LinkedHashMap<Integer, String>();
+
+	static {
+		correspondanceArabeRomain.put(1000, "M");
+		correspondanceArabeRomain.put(900, "CM");
+		correspondanceArabeRomain.put(500, "D");
+		correspondanceArabeRomain.put(400, "CD");
+		correspondanceArabeRomain.put(100, "C");
+		correspondanceArabeRomain.put(90, "XC");
+		correspondanceArabeRomain.put(50, "L");
+		correspondanceArabeRomain.put(40, "XL");
+		correspondanceArabeRomain.put(10, "X");
+		correspondanceArabeRomain.put(9, "IX");
+		correspondanceArabeRomain.put(5, "V");
+		correspondanceArabeRomain.put(4, "IV");
+		correspondanceArabeRomain.put(1, "I");
+	}
+
+	public static String converti(int arabe) {
+		String romain = StringUtils.EMPTY;
+		for (Integer valeur : correspondanceArabeRomain.keySet()) {
+			while (arabe >= valeur) {
+				romain += correspondanceArabeRomain.get(valeur);
+				arabe -= valeur;
+			}
+		}
+		return romain;
+	}
 }
--- src/test/java/kata/roman/numerals/ConvertisseurTest.java
+++ src/test/java/kata/roman/numerals/ConvertisseurTest.java
@@ -1,6 +1,46 @@
 package kata.roman.numerals;
 
+import static kata.roman.numerals.Convertisseur.converti;
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
 
 public class ConvertisseurTest {
 
+	@Test
+	public void peutConvertirDesChiffres() {
+		valideLaConversion(1, "I");
+		valideLaConversion(5, "V");
+		valideLaConversion(10, "X");
+		valideLaConversion(50, "L");
+		valideLaConversion(100, "C");
+		valideLaConversion(500, "D");
+		valideLaConversion(1000, "M");
+	}
+
+	@Test
+	public void peutConvertirLesAdditions() {
+		valideLaConversion(3, "III");
+		valideLaConversion(8, "VIII");
+		valideLaConversion(31, "XXXI");
+	}
+
+	@Test
+	public void peutConvertirLesSoustractions() {
+		valideLaConversion(4, "IV");
+		valideLaConversion(9, "IX");
+		valideLaConversion(40, "XL");
+		valideLaConversion(90, "XC");
+		valideLaConversion(400, "CD");
+		valideLaConversion(900, "CM");
+	}
+
+	@Test
+	public void peutConvertir4888() {
+		valideLaConversion(4888, "MMMMDCCCLXXXVIII");
+	}
+
+	public void valideLaConversion(int arabe, String romain) {
+		assertThat(converti(arabe)).isEqualTo(romain);
+	}
 }
